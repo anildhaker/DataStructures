8699ced09942313517aad562b1d2553bbcce135b
anildhaker <anildhaker777@gmail.com>
2019-02-24 22:36:17 +0530

    first commit

--- CircularLinkedList/SortedInsert.py
+++ CircularLinkedList/SortedInsert.py
@@ -32,10 +32,32 @@ class CircularLinkedList:
         temp = self.head 
         if self.head is not None: 
             while(True): 
-                print ("%d" %(temp.data), )
+                print ("%d" %(temp.data),)
                 temp = temp.next
                 if (temp == self.head): 
-                    break 
+                    break
+    
+    def insertSort(self, new_node):
+      cur = self.head
+      if cur is None:
+        new_node.next = new_node
+        self.head = new_node
+
+      elif (cur.data >= new_node.data):
+        while (cur.next != self.head):
+          cur = cur.next
+        cur.next = new_node
+        new_node.next = self.head
+        self.head = new_node
+
+      else:
+        while (cur.next.data < new_node.data):
+          cur = cur.next
+          if (cur == self.head):
+            break
+        new_node.next = cur.next
+        cur.next = new_node
+
   
   
 # Driver program to test above function 
@@ -44,10 +66,15 @@ class CircularLinkedList:
 cllist = CircularLinkedList() 
   
 # Created linked list will be 11->2->56->12 
-cllist.push(12) 
-cllist.push(56) 
-cllist.push(2) 
-cllist.push(11) 
-  
+cllist.push(40) 
+cllist.push(30) 
+cllist.push(20) 
+cllist.push(10)
 
+x = Node(25)
+
+
+cllist.printList()
+print
+cllist.insertSort(x)
 cllist.printList() 
