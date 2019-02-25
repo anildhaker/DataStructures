44c94ba007ab75c4675e36950cc89926b248a2a7
anildhaker <anildhaker777@gmail.com>
2019-02-25 13:08:25 +0530

    first commit

---
+++ Stack/QueueUsingStacks.py
@@ -0,0 +1,28 @@
+class Queue():
+  def __init__(self):
+    self.inStack = []
+    self.outStack = []
+
+  def enQueue(self,e):
+    self.inStack.append(e)
+
+  def deQueue(self):
+    if not self.outStack:
+      while self.inStack:
+        self.outStack.append(self.inStack.pop())
+    return self.outStack.pop()
+
+q = Queue()  
+
+for i in range(1, 11):
+  q.enQueue(i)
+
+
+
+print
+
+for i in range(10):
+  print (q.deQueue())
+
+    
+
