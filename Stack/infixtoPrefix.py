6412dfc44ae7956345ba2c4d9479c3c758415aae
anildhaker <anildhaker777@gmail.com>
2019-03-10 01:30:56 +0530

    first commit

--- Stack/infixtoPrefix.py
+++ Stack/infixtoPrefix.py
@@ -31,6 +31,56 @@ class Conversion:
 
     
     def is_Operand(self, char):
+      return char.isalpha()
+
+    # check the precedence of operator --> is it strictly lesser than 
+
+    def notGreater(self, i):
+      a = self.precedence[i]
+      b = self.precedence[self.peek()]
+      return True if a <= b else False
+
+
+    def reverseExp(self, exp):
+      reversedExp = exp[::-1]
+      list_reverse_exp = list(reversedExp)
+
+      for i, c in enumerate(list_reverse_exp):
+            if list_reverse_exp[i] == '(':
+                list_reverse_exp[i] = ')'
+            elif list_reverse_exp[i] == ')':
+                list_reverse_exp[i] = '('
+      s = ''.join(list_reverse_exp)
+      return s
+
+    def infixToPrefix(self, exp):
+        reversed_exp = self.reverse(exp)
+        for i in reversed_exp:
+            if self.isOperand(i):
+                self.output.append(i)
+            elif i == '(':
+                self.push(i)
+            elif i == ')':
+                while not self.isEmpty() and self.peek() != '(':
+                    a = self.pop()
+                    self.output.append(a)
+                if not self.isEmpty() and self.peek() != '(':
+                    return -1
+                else:
+                    self.pop()
+            else:
+                while not self.isEmpty() and self.notGreater(i):
+                    self.output.append(self.pop())
+                self.push(i)
+        while not self.isEmpty():
+            self.output.append(self.pop())
+        temp = "".join(self.output)
+        infix_to_prefix_expression = self.reverse(temp)
+        print(infix_to_prefix_expression)
+      
+
+
+
       
 
     
