49231ace0f06a3a479d231df77c2756f2726b84c
anildhaker <anildhaker777@gmail.com>
2019-11-18 23:07:53 +0530

    mid element

---
+++ SinglyLinkedList/printMidNode.py
@@ -0,0 +1,33 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None 
+
+class LinkedList: 
+  
+    def __init__(self): 
+        self.head = None
+    
+    def push(self, new_data): 
+        new_node = Node(new_data) 
+        new_node.next = self.head 
+        self.head = new_node
+
+    def midElement(self):
+      slow = self.head
+      fast = self.head
+      
+      while(fast != None and fast.next!= None):
+        slow = slow.next
+        fast = fast.next.next
+
+      print(slow.data)
+        
+llist = LinkedList() 
+llist.push(1) 
+llist.push(3) 
+llist.push(100) 
+llist.push(2) 
+llist.push(1)
+
+llist.midElement()
