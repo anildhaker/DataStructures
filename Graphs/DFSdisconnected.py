e333fa5b95ecfc08a036bbeadfc4244f64361d7d
anildhaker <anildhaker777@gmail.com>
2019-11-25 02:51:03 +0530

    DFS Disconnected

---
+++ Graphs/DFSdisconnected.py
@@ -0,0 +1,37 @@
+# DFS for disconnected Graph
+from __future__ import print_function
+from collections import defaultdict
+
+class Graph:
+  def __init__(self):
+    self.graph = defaultdict(list)
+
+  def addEdge(self, u, v):
+    self.graph[u].append(v)
+
+  def DFSUtil(self, v, visited):
+    visited[v]= True
+    print(v,end=" ")
+
+    for i in self.graph[v]: 
+      if visited[i] == False:
+        self.DFSUtil(i, visited) 
+
+  def DFS(self):
+    V = len(self.graph)
+    visited = [False] * (V)
+    
+    for i in range(V): 
+      if visited[i] == False: 
+        self.DFSUtil(i, visited) 
+    
+g = Graph() 
+g.addEdge(0, 1) 
+g.addEdge(0, 2) 
+g.addEdge(1, 2) 
+g.addEdge(2, 0) 
+g.addEdge(2, 3) 
+g.addEdge(3, 3)
+
+print ("Depth First Traversal--->")
+g.DFS() 
